4b02f4907e97478e0aacf73776bc07ce569ab417
micheal olayanju <olayanjumbennedict@gmail.com>
2022-08-21 09:57:19 +0000

    god updates4

--- god.java
+++ god.java
@@ -1,3 +1,9 @@
  gbuoiegergnergomerfg
  life with God is full of blessings, trust him and everything would be fine 
+<<<<<<< HEAD
  thank you lord 
+=======
+ thank you lord
+his grace is sufficien
+rrrrrrrrrrrrrrrrt 
+>>>>>>> f8b10c1 (god new)
